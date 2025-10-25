d11c816977c9c57c99b6f708d05e083a641db2ca
ToMZanG <63664294+tomzangsad@users.noreply.github.com>
2025-10-25 11:02:54 +0700

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -763,7 +763,7 @@ jq -r '.[] | select(.generated == false) | [.path_hash, .path, .model_name] | @t
     texture_path=$(echo "$texture_path" | sed 's|^textures/|textures/zicon/|')
 
     # ✅ ตัด suffix (_cosmetic, _cosmetic_self, _normal_1, _normal_2, _self) ออกก่อนบันทึก
-    texture_path=$(echo "$texture_path" | sed -E 's/(_cosmetic(_self)?|_normal(_[0-9]+|_[0-9]+)?|_self)\.png$/.png/')
+    texture_path=$(echo "$texture_path" | sed -E 's/(_(cosmetic(_self)?|normal(_[0-9]+)?|self|[0-9]+))\.png$/.png/I')
 
     # ✅ สร้างโฟลเดอร์ตาม path นั้นจริงใน ./target/rp/
     texture_dir="./target/rp/$(dirname "$texture_path")"
