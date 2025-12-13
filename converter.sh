314e645295cfbf7159bde36dcf6a20c2cca836e8
dssdjjtt2345 <63664294+tomzangsad@users.noreply.github.com>
2025-12-13 09:08:53 +0700

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -1120,7 +1120,8 @@ do
 
         # MULTI KEYWORD CHECK
         #if [[ "$model_lower" =~ (wing|tail|backpack|shield) ]]; then
-		if [[ "${model_name,,}" == *"wing"* ]] || [[ "${model_name,,}" == *"back"* ]] || [[ "${model_name,,}" == *"backpack"* ]] || [[ "${model_name,,}" == *"shield"* ]]; then
+		if [[ "${model_name,,}" =~ (wing|back|backpack|shield) ]]; then
+		#if [[ "${model_name,,}" == *"wing"* ]]; then
         # ------------------------------------------
         #  WING SPECIAL ANIMATION
         # ------------------------------------------
