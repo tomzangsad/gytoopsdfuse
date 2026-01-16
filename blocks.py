90c3832d39642c2e7d4b164e789bf15113778da7
dssdjjtt2345 <63664294+tomzangsad@users.noreply.github.com>
2026-01-16 11:53:39 +0700

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -4,23 +4,30 @@ import os
 import blocks_util
 
 #Not Support For Fire
+print(f"[INFO] Scanning for blockstates...")
 files = glob.glob("pack/assets/minecraft/blockstates/*.json")
+print(f"[INFO] Found {len(files)} blockstate files found.")
 if files != []:
     blocks_util.write_animated_cube()
     blocks_util.write_geometry_cube()
 for file in files:
     file = file.replace("\\", "/")
     block = (file.split("/")[-1]).replace(".json", "")
-    if (block == "fire"): continue
+    print(f"\n[INFO] Processing block: {block} ({file})")
+    if (block == "fire"): 
+        print(f"[INFO] Skipping blocked block: {block}")
+        continue
     block_material = os.getenv("BLOCK_MATERIAL")
     blocks_util.write_mapping_block(block)
     with open(file, "r") as f:
         data = json.load(f)["variants"]
         for k, v in data.items():
             if not("block/original" in v["model"] or "block/tripwire_attached_n" in v["model"]):
-                print(v["model"])
+                print(f"  [DEBUG] Processing Variant: {k} | Model: {v['model']}")
                 am = blocks_util.get_am_file(v["model"])
-                if am == None: continue
+                if am == None: 
+                    print(f"  [WARN] AM file not found for model: {v['model']} - Skipping.")
+                    continue
                 with open(am, "r") as f:
                     data_am = json.load(f)
                     gmdl = data_am["minecraft:attachable"]["description"]["identifier"].split(":")[1]
@@ -35,3 +42,4 @@ for file in files:
                     sstate = k.split(",")
                     k = f"{sstate[0]},{sstate[4]},{sstate[1]},{sstate[2]},{sstate[6]},{sstate[3]},{sstate[5]}"
                 blocks_util.regsister_block(block, gmdl, k, texture, block_material, geometry)
+                print(f"  [SUCCESS] Registered variant: {k}")
